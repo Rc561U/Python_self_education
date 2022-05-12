5c61ae086340b6f90903dccc07143f2bf4e889b3
Anton_Ramancou <60200520+Rc561U@users.noreply.github.com>
2022-05-12 21:27:04 +0300

    Try to make push into main branch (#3)
    1. Created understandable print output
    2. Replaced names variables
    3. Added new boolean condition
    4. Practicing with squash and merge

---
+++ coursera.py
@@ -0,0 +1,41 @@
+import os
+import shutil
+import sys
+
+def checkDiskSpace():
+    '''Returns True if the computer has a pending reboot'''
+    return os.path.exists('/run/reboot-required')
+
+def checkDiskFull(disk, min_gb, min_percent):
+    '''Returns True if there isn't enough disk space, False otherwise.'''
+    du = shutil.disk_usage(disk)
+    #calculate the percentage of free space
+    percent_free = 100 * du.free / du.total
+    gigabytes_free = du.free / 2 **30
+    if gigabytes_free <= min_gb or percent_free <= min_percent:
+        return True
+    return False
+
+def checkRootFull():
+    '''Returns True if root partition is full, False otherwise'''
+    return checkDiskFull(disk='/', min_gb=2, min_percent=10)
+
+def main():
+    outputResult = True
+    checks = [
+            (checkDiskSpace, 'Pendig Reboot'),
+            (checkRootFull, 'Root partition full'),
+    ]
+
+    for check, msg in checks:
+        if check():
+            print(msg)
+            outputResult = False
+
+    if not outputResult:
+        sys.exit(1)
+
+    print('Test successfully complited. Your disk root space is in optimal condition (default values is 2GB and 10%)')
+    sys.exit(0)
+
+main()
