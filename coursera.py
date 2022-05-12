8a87218ad0905348ba8a077d9c90d36e8e391fc5
Anton_Ramancou <60200520+Rc561U@users.noreply.github.com>
2022-05-12 20:11:16 +0300

    Update coursera.py

--- coursera.py
+++ coursera.py
@@ -6,36 +6,36 @@ def checkDiskSpace():
     '''Returns True if the computer has a pending reboot'''
     return os.path.exists('/run/reboot-required')
 
-def check_disk_full(disk, min_gb, min_percent):
+def checkDiskFull(disk, min_gb, min_percent):
     '''Returns True if there isn't enough disk space, False otherwise.'''
     du = shutil.disk_usage(disk)
     #calculate the percentage of free space
     percent_free = 100 * du.free / du.total
     gigabytes_free = du.free / 2 **30
-    if gigabytes_free < min_gb or percent_free < min_percent:
+    if gigabytes_free <= min_gb or percent_free <= min_percent:
         return True
     return False
 
-def check_root_full():
-    '''Returns True is root partition is full, False otherwise'''
-    return check_disk_full(disk='/', min_gb=2, min_percent=10)
+def checkRootFull():
+    '''Returns True if root partition is full, False otherwise'''
+    return checkDiskFull(disk='/', min_gb=2, min_percent=10)
 
 def main():
-    everything_ok = True
+    outputResult = True
     checks = [
             (checkDiskSpace, 'Pendig Reboot'),
-            (check_root_full, 'Root partition full'),
+            (checkRootFull, 'Root partition full'),
     ]
 
     for check, msg in checks:
         if check():
             print(msg)
-            everything_ok = False
+            outputResult = False
 
-    if not everything_ok:
+    if not outputResult:
         sys.exit(1)
 
-    print('Everything ok. Your disk space is optimal ( compare with 2GB and 10% )')
+    print('Test successfully complited. Your disk root space is in optimal condition (default values is 2GB and 10%)')
     sys.exit(0)
 
 main()
