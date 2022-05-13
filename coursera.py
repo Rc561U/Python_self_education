ef546b1733e03836708e7c0b9984cb818305de40
Rc561U <hugeanton29011994@gmail.com>
2022-05-13 19:28:20 +0300

    Add simple network connectivity check

--- coursera.py
+++ coursera.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import socket
 import sys
 
 def checkDiskSpace():
@@ -20,11 +21,19 @@ def checkRootFull():
     '''Returns True if root partition is full, False otherwise'''
     return checkDiskFull(disk='/', min_gb=2, min_percent=10)
 
+def checkNoNetwork():
+    """Returns True if it fails to resolve Google's URL, False otherwise"""
+    try:
+        socket.gethostbyname('www.google.com')
+        return False
+    except:
+        return True
 def main():
     outputResult = True
     checks = [
-            (checkDiskSpace, 'Pendig Reboot'),
-            (checkRootFull, 'Root partition full'),
+        (checkDiskSpace, 'Pendig Reboot'),
+        (checkRootFull, 'Root partition full'),
+        (checkNoNetwork, 'No working network'),
     ]
 
     for check, msg in checks:
@@ -35,7 +44,7 @@ def main():
     if not outputResult:
         sys.exit(1)
 
-    print('Test successfully complited. Your disk root space is in optimal condition (default values is 2GB and 10%)')
+    print('Test successfully completed.\nYour disk root space is in optimal condition (default values is 2GB and 10%)')
     sys.exit(0)
 
 main()
