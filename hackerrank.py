d356094e04475ce7cdc4a5f06e56540686dec32f
Rc561U <hugeanton29011994@gmail.com>
2022-05-27 00:12:48 +0300

    Try to add new file

---
+++ hackerrank.py
@@ -0,0 +1,11 @@
+def solving(x):
+    result = []
+    for i in range(1,11):
+        result.append(f'{x} * {i} = {x*i}')
+    return result
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    res = (solving(n))
+    for i in res:
+        print(i)
