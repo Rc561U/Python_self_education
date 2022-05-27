211348f743377b55015603218ceca8e0d83b21f2
Rc561U <hugeanton29011994@gmail.com>
2022-05-27 22:50:09 +0300

    Try to add new file

---
+++ Codewars/Fibonacci_Rabbits .py
@@ -0,0 +1,14 @@
+from functools import lru_cache
+
+
+@lru_cache
+def fib_rabbits(x, b):
+    # your code here
+    return fib_rabbits(x - 1, b) + b * fib_rabbits(x - 2, b) if x > 1 else x
+
+
+def Fibonacci_Loop_Py(months, offsprings):
+    parrent, child = 1, 1
+    for i in range(months - 1):
+        child, parrent = parrent, parrent + (child * offsprings)
+    return child
