f20ad84ca5d00351c8daa18f9dce59e22c3fe261
Rc561U <hugeanton29011994@gmail.com>
2022-06-01 10:05:53 +0300

    Coursera week 6

---
+++ Coursera/EditingFilesUsingSubstrings/changeJane.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import sys
+import subprocess
+with open(sys.argv[1], 'r') as f:
+    content1 = f.readlines()
+    content2 = [x.strip() for x in content1]
+    content3 = [item.replace("jane", "jdoe") for item in content2]
+    res = dict(zip(content2, content3))
+    for key, value in res.items():
+        subprocess.run(['mv', 'username'+key, 'username'+value])
